708277d7c22938d279f3c9e050cf014e5d2de1e6
paritydiscount <109464362+paritydiscount@users.noreply.github.com>
2022-07-17 17:03:55 +0530

    hello world - first commit

---
+++ parityDiscount.js
@@ -0,0 +1,14 @@
+!function () {
+  document.head.appendChild(
+    <div
+      style="
+          color: white;
+          font-size: 17px;
+          background-color: #0b66dbcc;
+          padding: 18px 22px;
+        "
+    >
+      Get a parity discount!
+    </div>
+  );
+};

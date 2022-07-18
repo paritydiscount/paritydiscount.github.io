a7437500130032fa9426805fbed3793a74d0b2e1
paritydiscount <109464362+paritydiscount@users.noreply.github.com>
2022-07-18 21:19:45 +0530

    "<" issue fixed added

--- parityDiscount.js
+++ parityDiscount.js
@@ -1,14 +1,10 @@
 !function () {
-  document.head.appendChild(
-    <div
-      style="
-          color: white;
-          font-size: 17px;
-          background-color: #0b66dbcc;
-          padding: 18px 22px;
-        "
-    >
-      Get a parity discount!
-    </div>
+  console.log("enter");
+  var z = document.createElement("div"); // is a node
+  z.setAttribute(
+    "style",
+    "color: white;font-size: 17px;background-color: #0b66dbcc;padding: 18px 22px;"
   );
+  z.innerHTML = "Get a parity discount!";
+  document.head.appendChild(z);
 };

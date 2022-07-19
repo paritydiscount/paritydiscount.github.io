5635a776b9836f6aecf4ed98bff3f02215dc149e
paritydiscount <109464362+paritydiscount@users.noreply.github.com>
2022-07-19 20:29:53 +0530

    alert box added

--- parityDiscount.js
+++ parityDiscount.js
@@ -1,4 +1,5 @@
 !function () {
+  alert ("This is an alert dialog box");  
   console.log("enter");
   var z = document.createElement("div"); // is a node
   z.setAttribute(
@@ -6,5 +7,5 @@
     "color: white;font-size: 17px;background-color: #0b66dbcc;padding: 18px 22px;"
   );
   z.innerHTML = "Get a parity discount!";
-  document.head.appendChild(z);
+  document.body.appendChild(z);
 };

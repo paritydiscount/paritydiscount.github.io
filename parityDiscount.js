80ba52d96dd841300e4d194f6950a57da73f5f10
paritydiscount <109464362+paritydiscount@users.noreply.github.com>
2022-07-23 20:02:18 +0530

    console added in parity discount

--- parityDiscount.js
+++ parityDiscount.js
@@ -8,14 +8,19 @@ window.onload = function (){
 //   document.body.appendChild(z);
   
   
-    var t = ".parity-banner {background-color:#0b66dbcc;color:white;border-radius:10px;font-size: 1rem;padding: 20px 10px;text-align: center;position: relative;}",
-        r = ".parity-banner-close-btn{width:1rem;height:1rem;border:0;opacity:.5;background-color:transparent;color:currentColor;position:absolute;top:1rem;right:1rem;padding:0}.parity-banner-close-btn:hover{opacity:1;}",
-        o = document.createElement("style");
+    var t = ".parity-banner {background-color:#0b66dbcc;color:white;border-radius:10px;font-size: 1rem;padding: 20px 10px;text-align: center;position: relative;}",r = ".parity-banner-close-btn{width:1rem;height:1rem;border:0;opacity:.5;background-color:transparent;color:currentColor;position:absolute;top:1rem;right:1rem;padding:0}.parity-banner-close-btn:hover{opacity:1;}",o = document.createElement("style");
 
-    	o.innerText = t + r + ".parity-banner.parity-banner-has-logo {padding-left: 120px;}", document.head.appendChild(o);
+		console.log({t,r,o});
+	
+    	o.innerText = t + r;
+	document.head.appendChild(o);
+	
+	console.log({o});
   
     var a = "Hello 🇮🇳 Hey! It looks like you are from India. We support Parity Purchasing Power, so if you need it, use code “DFG56ED” to get 50% off your subscription at checkout.";
 		
+		console.log({a});
+	
     document.getElementsByClassName("parity-banner").length || ("top" === "top" ? document.body.insertAdjacentHTML("afterbegin", a) : document.body.insertAdjacentHTML("beforeend", a));
   
     	var i = document.querySelector(".parity-banner-close-btn");

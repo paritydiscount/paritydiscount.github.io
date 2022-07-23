7f452cc909339f95db3670f6dfa1344459897b2c
paritydiscount <109464362+paritydiscount@users.noreply.github.com>
2022-07-23 20:05:42 +0530

    parity discount file added

--- parityDiscount.js
+++ parityDiscount.js
@@ -8,24 +8,52 @@ window.onload = function (){
 //   document.body.appendChild(z);
   
   
-    var t = ".parity-banner {background-color:#0b66dbcc;color:white;border-radius:10px;font-size: 1rem;padding: 20px 10px;text-align: center;position: relative;}",r = ".parity-banner-close-btn{width:1rem;height:1rem;border:0;opacity:.5;background-color:transparent;color:currentColor;position:absolute;top:1rem;right:1rem;padding:0}.parity-banner-close-btn:hover{opacity:1;}",o = document.createElement("style");
+//     var t = ".parity-banner {background-color:#0b66dbcc;color:white;border-radius:10px;font-size: 1rem;padding: 20px 10px;text-align: center;position: relative;}",r = ".parity-banner-close-btn{width:1rem;height:1rem;border:0;opacity:.5;background-color:transparent;color:currentColor;position:absolute;top:1rem;right:1rem;padding:0}.parity-banner-close-btn:hover{opacity:1;}",o = document.createElement("style");
 
-		console.log({t,r,o});
+// 		console.log({t,r,o});
 	
-    	o.innerText = t + r;
-	document.head.appendChild(o);
+//     	o.innerText = t + r;
+// 	document.head.appendChild(o);
 	
-	console.log({o});
+// 	console.log({o});
   
-    var a = "Hello 🇮🇳 Hey! It looks like you are from India. We support Parity Purchasing Power, so if you need it, use code “DFG56ED” to get 50% off your subscription at checkout.";
+//     var a = "Hello 🇮🇳 Hey! It looks like you are from India. We support Parity Purchasing Power, so if you need it, use code “DFG56ED” to get 50% off your subscription at checkout.";
 		
-		console.log({a});
+// 		console.log({a});
 	
-    document.getElementsByClassName("parity-banner").length || ("top" === "top" ? document.body.insertAdjacentHTML("afterbegin", a) : document.body.insertAdjacentHTML("beforeend", a));
+//     document.getElementsByClassName("parity-banner").length || ("top" === "top" ? document.body.insertAdjacentHTML("afterbegin", a) : document.body.insertAdjacentHTML("beforeend", a));
   
-    	var i = document.querySelector(".parity-banner-close-btn");
+//     	var i = document.querySelector(".parity-banner-close-btn");
+// 			i && i.addEventListener("click", function() {
+// 				var e = document.querySelector(".parity-banner");
+// 				e.parentNode.removeChild(e)
+//       })
+
+
+	var e = new XMLHttpRequest;
+	e.open("GET", "https://api.paritydeals.com/api/v1/deals/discount/?url=".concat(window.location.href), !0);
+	var n = "Oops! something went wrong. Please email contact@paritydeals.com";
+	e.onload = function() {
+		if (this.status >= 200 && this.status < 400) {
+			var e = JSON.parse(this.response);
+			if (!e.bar) return;
+			var t = e.bar.unStyled ? ".parity-banner {background-color: ".concat(e.bar.backgroundColor, ";color: ").concat(e.bar.fontColor, ";border-radius: ").concat(e.bar.borderRadius, ";font-size: 1rem;padding: 20px 10px;text-align: center;position: relative;}") : "",
+				r = e.bar.addCloseIcon ? ".parity-banner-close-btn{width:1rem;height:1rem;border:0;opacity:.5;background-color:transparent;color:currentColor;position:absolute;top:1rem;right:1rem;padding:0}.parity-banner-close-btn:hover{opacity:1;}" : "",
+				o = document.createElement("style");
+			o.innerText = t + r + ".parity-banner.parity-banner-has-logo {padding-left: 120px;}", document.head.appendChild(o);
+			console.log({t,r,o});
+			var a = e.message;
+			console.log({a});
+			document.getElementsByClassName("parity-banner").length || ("top" === e.bar.placement ? document.body.insertAdjacentHTML("afterbegin", a) : document.body.insertAdjacentHTML("beforeend", a));
+			var i = document.querySelector(".parity-banner-close-btn");
 			i && i.addEventListener("click", function() {
 				var e = document.querySelector(".parity-banner");
 				e.parentNode.removeChild(e)
-      })
+			})
+		} else console.log(n)
+	}, e.onerror = function() {
+		console.log(n)
+	}, e.send()
+
+
 };
